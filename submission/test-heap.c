424a3d2a9c159b99a4c7dfcaa5e1ba28bb01478e
Sean Gillen <sdgillen@crimson.ua.edu>
2018-03-07 12:49:03 -0600

    trying to fix it

--- submission/test-heap.c
+++ submission/test-heap.c
@@ -1,83 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "heap.h"
+#include "bst.h"
 #include "integer.h"
+#include "real.h"
+#include "string.h"
+#include "stack.h"
+#include "queue.h"
+#include "heap.h"
 
-int rcompareINTEGER(void *a, void *b) {
-	return -1 * compareINTEGER(a, b);
-}
+extern void srandom(unsigned int);
+extern long int random(void);
 
-int main() {
-	HEAP *t = newHEAP(displayINTEGER, compareINTEGER, freeINTEGER);
-	displayHEAP(t, stdout);
-	fputc('\n',stdout);
-	displayHEAPdebug(t, stdout);
-	fputc('\n',stdout);
-	INTEGER *x = newINTEGER(68);
-	insertHEAP(t, x);
-	x = newINTEGER(54);
-	insertHEAP(t, x);
-	x = newINTEGER(53);
-	displayHEAP(t, stdout);
-	fputc('\n',stdout);
-	displayHEAPdebug(t, stdout);
-	fputc('\n',stdout);
-	insertHEAP(t, x);
-	displayHEAP(t, stdout);
-	fputc('\n',stdout);
-	displayHEAPdebug(t, stdout);
-	fputc('\n',stdout);
-	x = newINTEGER(45);
-	insertHEAP(t, x);
-	x = newINTEGER(48);
-	insertHEAP(t, x);
-	x = newINTEGER(50);
-	insertHEAP(t, x);
-	x = newINTEGER(55);
-	insertHEAP(t, x);
-	x = newINTEGER(74);
-	insertHEAP(t, x);
-	x = newINTEGER(70);
-	insertHEAP(t, x);
-	x = newINTEGER(75);
-	insertHEAP(t, x);
-	x = newINTEGER(80);
-	insertHEAP(t, x);
-	displayHEAP(t, stdout);
-	fputc('\n',stdout);
-	displayHEAPdebug(t, stdout);
-	fputc('\n',stdout);
-
-	buildHEAP(t);
-	displayHEAP(t, stdout);
-	fputc('\n', stdout);
-	displayHEAPdebug(t, stdout);
-	fputc('\n', stdout);
-
-	displayINTEGER(peekHEAP(t), stdout);
-	fputc('\n', stdout);
-
-	x = extractHEAP(t);
-	displayINTEGER(x, stdout);
-	fputc('\n', stdout);
-	displayHEAP(t, stdout);
-	fputc('\n', stdout);
-	displayHEAPdebug(t, stdout);
-	fputc('\n', stdout);
-	freeINTEGER(x);
-
-	x = extractHEAP(t);
-	displayINTEGER(x, stdout);
-	fputc('\n', stdout);
-	displayHEAP(t, stdout);
-	fputc('\n', stdout);
-	displayHEAPdebug(t, stdout);
-	fputc('\n', stdout);
-
-	displayINTEGER(peekHEAP(t), stdout);
-	fputc('\n', stdout);
-
-	freeHEAP(t);
-	freeINTEGER(x);
-	return 0;
-}
+int
+main(void)
+    {
+    srandom(2001);
+    //simple INTEGER test of HEAP
+    HEAP *p = newHEAP(displayINTEGER,compareINTEGER,freeINTEGER);
+    insertHEAP(p,newINTEGER(3));
+    insertHEAP(p,newINTEGER(0));
+    insertHEAP(p,newINTEGER(2));
+    buildHEAP(p);
+    //printf("heap: ");
+    //displayHEAP(p,stdout);
+    //printf("\n");
+    //printf("debug:\n");
+    //displayHEAPdebug(p,stdout);
+    printf("min should be %d\n",getINTEGER(peekHEAP(p)));
+    INTEGER *r = extractHEAP(p);
+    printf("min is %d\n",getINTEGER(r));
+    freeINTEGER(r);
+    //printf("heap: ");
+    //displayHEAP(p,stdout);
+    //printf("\n");
+    //printf("debug:\n");
+    //displayHEAPdebug(p,stdout);
+    printf("min should be %d\n",getINTEGER(peekHEAP(p)));
+    r = extractHEAP(p);
+    printf("min is %d\n",getINTEGER(r));
+    freeINTEGER(r);
+    //printf("heap: ");
+    //displayHEAP(p,stdout);
+    //printf("\n");
+    //printf("debug:\n");
+    //displayHEAPdebug(p,stdout);
+    printf("peek: %d\n",getINTEGER(peekHEAP(p)));
+    printf("size: %d\n",sizeHEAP(p));
+    freeHEAP(p);
+    return 0;
+    }
