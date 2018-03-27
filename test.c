170d403f07f9049b0f5e5aa9a4eb9fa9ef210769
tortoise <tortoisewrath@tortoisewrath.com>
2018-03-27 08:39:40 -0500

    before fix attempt

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bst.h"
-#include "gst.h"
 #include "integer.h"
 #include "real.h"
 #include "string.h"
+#include "queue.h"
 
 void srandom(unsigned int);
 long int random(void);
@@ -12,44 +12,49 @@ long int random(void);
 int
 main(void)
     {
-    srandom(1);
-    //simple INTEGER test of GST
-    GST *p = newGST(displayINTEGER,compareINTEGER,freeINTEGER);
-    insertGST(p,newINTEGER(3));
-    insertGST(p,newINTEGER(2));
-    printf("GST:\n");
-    displayGST(p,stdout);
-    printf("debug: ");
-    displayGSTdebug(p,stdout);
-    printf("\n");
-    INTEGER *q = newINTEGER(2);
-    printf("is ");
-    displayINTEGER(q,stdout);
-    printf(" present? %s\n",findGST(p,q) == 0? "no" : "yes");
-    INTEGER *r = newINTEGER(3);
-    freeINTEGER(deleteGST(p,r));
-    printf("GST:\n");
-    displayGST(p,stdout);
-    printf("debug: ");
-    displayGSTdebug(p,stdout);
-    printf("\n");
-    printf("is ");
-    displayINTEGER(q,stdout);
-    printf(" present? %s\n",findGST(p,q) == 0? "no" : "yes");
-    setINTEGER(r,2);
-    freeINTEGER(deleteGST(p,r));
-    printf("GST:\n");
-    displayGST(p,stdout);
-    printf("debug: ");
-    displayGSTdebug(p,stdout);
-    printf("\n");
-    printf("is ");
-    displayINTEGER(q,stdout);
-    printf(" present? %s\n",findGST(p,q) == 0? "no" : "yes");
-    printf("size: %d\n",sizeGST(p));
-    statisticsGST(p,stdout);
-    freeINTEGER(q);
-    freeINTEGER(r);
-    freeGST(p);
+    srandom(201);
+    //simple INTEGER test of BST
+    int i;
+    BST *p = newBST(displayINTEGER,compareINTEGER,0,freeINTEGER);
+    for (i = 0; i < 10000; ++i)
+        {
+        int j = random() % 10000;
+        INTEGER *a = newINTEGER(j);
+        if (findBST(p,a) == 0)
+            insertBST(p,a);
+        else
+            freeINTEGER(a);
+        }
+    if (sizeBST(p) < 200)
+        {
+        printf("pre-order: ");
+        displayBST(p,stdout);
+        printf("\n");
+        printf("level: ");
+        displayBSTdebug(p,stdout);
+        }
+    printf("size is %d\n",sizeBST(p));
+    while (sizeBST(p) > 200)
+        {
+        for (i = 0; i < 10000; ++i)
+            {
+            int j = random() % 10000;
+            INTEGER *a = newINTEGER(j);
+            if (findBST(p,a) != 0)
+                freeBSTNODE(deleteBST(p,a),freeINTEGER);
+            freeINTEGER(a);
+            }
+        }
+    if (sizeBST(p) < 200)
+        {
+        printf("pre-order: ");
+        displayBST(p,stdout);
+        printf("\n");
+        printf("level: ");
+        displayBSTdebug(p,stdout);
+        }
+    printf("size is %d\n",sizeBST(p));
+    statisticsBST(p,stdout);
+    freeBST(p);
     return 0;
     }
